dedf7622a5a92c302939678eb6c9e3a18aad0c29
DroidZed <41507665+DroidZed@users.noreply.github.com>
2024-02-08 13:59:06 +0100

    🎉Setup Project

---
+++ app/src/main/java/tn/droidzed/mywifi/MainActivity.kt
@@ -0,0 +1,54 @@
+package tn.droidzed.mywifi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import tn.droidzed.mywifi.ui.theme.MyWiFiTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyWiFiTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("DroidZed")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyWiFiTheme {
+        Greeting("Android")
+    }
+}
